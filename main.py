07c569a1a51867a27c9aad9b5532d5b8fc33dfe1
Kuba Karasek <jakkarasek@gmail.com>
2026-09-17 15:48:44 +0200

    add router

--- main.py
+++ main.py
@@ -1,30 +1,7 @@
 from fastapi import FastAPI
+from app.routes.issues import router as issues_router
 
 app = FastAPI()
 
 
-items = [
-    {"id": 1, "name": "Apple 🍏"},
-    {"id": 2, "name": "Orange 🍊"},
-    {"id": 3, "name": "Pear 🍐"},
-]
-
-
-@app.get("/health")
-def health_check():
-    return {"status": "ok"}
-
-
-@app.get("/items")
-def get_items(page: int, limit: int):
-    return {"items": items,
-            "page": page,
-            "limit": limit}
-
-
-@app.get("/items/{item_id}")
-def get_item(item_id: int):
-    for item in items:
-        if item["id"] == item_id:
-            return item
-    return {"error": "Item not found"}
+app.include_router(issues_router)
