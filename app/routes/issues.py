1b0c124299e816848be1cf6b1d200c2d2712e846
Kuba Karasek <jakkarasek@gmail.com>
2026-09-21 13:27:47 +0200

    add POST route; GET fixes

--- app/routes/issues.py
+++ app/routes/issues.py
@@ -1,8 +1,30 @@
-from fastapi import APIRouter
+import uuid
+from fastapi import APIRouter, HTTPException, status
+from starlette.status import HTTP_201_CREATED
+from app.schemas import IssueCreate, IssueOut, IssueStatus
+from app.storage import load_data, save_data
 
 router = APIRouter(prefix="/api/v1/issues", tags=["issues"])
 
 
-@router.get("/")
+@router.get("/", response_model=list[IssueOut])
 async def get_issues():
-    return []
+    """Retrieve all issues."""
+    issues = load_data()
+    return issues
+
+
+@router.post("/", response_model=IssueOut, status_code=status.HTTP_201_CREATED)
+def create_issue(payload: IssueCreate):
+    """Create a new issue."""
+    issues = load_data()
+    new_issue = {
+        "id": str(uuid.uuid4()),
+        "title": payload.title,
+        "description": payload.description,
+        "priority": payload.priority,
+        "status": IssueStatus.open
+    }
+    issues.append(new_issue)
+    save_data(issues)
+    return new_issue
